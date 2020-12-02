38cff76184b675daf3551018451ee899677c84e6
Ewoud Dronkert <ewoud@dronkert.net>
2020-12-02 13:36:53 +0100

    comments

--- day02.py
+++ day02.py
@@ -5,15 +5,22 @@ with open('input02.txt', 'rt') as f:
 
 bad = 0
 good = 0
+
 for line in txt:
-	fields = line.split()
-	limit = list(map(int, fields[0].split('-')))
-	char = fields[1].strip(':')
-	pwd = fields[2]
-	n = pwd.count(char)
-	if n < limit[0] or n > limit[1]:
+	fld = line.split()
+	num = list(map(int, fld[0].split('-')))
+	ch = fld[1].strip(':')
+	pw = fld[2]
+
+	# Part 1
+	# char must occur from num0 to num1 times in string
+	n = pw.count(ch)
+	if n < num[0] or n > num[1]:
 		bad += 1
-	if (pwd[limit[0] - 1] == char) != (pwd[limit[1] - 1] == char):
+
+	# Part 2
+	# 1 of 2 chars at indices num0 and num1 (1-based) must be char
+	if (pw[num[0] - 1] == ch) != (pw[num[1] - 1] == ch):
 		good += 1
 
 print('bad:', bad)
