294ff09e1c9386293b0229b3b3b441c6439134f8
Ewoud Dronkert <ewoud@dronkert.net>
2020-12-03 16:22:53 +0100

    comments

--- day03.py
+++ day03.py
@@ -1,29 +1,27 @@
 #!/usr/bin/env python3
 
 with open('input03.txt', 'rt') as f:
-	treemap = list(f)
+	data = list(f)  # map of trees
 
-# Where are the trees in the input file
-def istree(x, y):
-	return 1 if treemap[y][x] == '#' else 0
+# Where are the trees in the input file?
+def tree(x, y):
+	return 1 if data[y][x] == '#' else 0
 
-dimx = len(treemap[0].strip())  # strip EOL '\n'
-dimy = len(treemap)             # number of lines
-prod = 1                        # answer to part 2
+w = len(data[0].strip())  # width = chars on a line minus EOL
+h = len(data)             # height = number of lines
+p = 1                     # product = answer to part 2
 
-# dx,dy pairs (3,1 was part 1)
+# All [dx,dy] pairs for part 2 (part 1 was [3,1])
 slopes = [[1,1], [3,1], [5,1], [7,1], [1,2]]
 
 for slope in slopes:
 	dx, dy = slope
-	x = 0
-	y = 0
-	trees = 0
-	while y < dimy:
-		trees += istree(x, y)
-		x = (x + dx) % dimx
-		y += dy
-	prod *= trees
-	print(dx, dy, trees)  # slope and number of trees
+	x = y = n = 0  # start at top-left, n = count the trees
+	while y < h:
+		n += tree(x, y)   # is there a tree here?
+		x = (x + dx) % w  # move horizontally
+		y += dy           # move vertically
+	p *= n                # multiply the numbers of trees
+	print(dx, dy, n)      # show slope and number of trees
 
-print(prod)  # product of the tree numbers on all the slopes
+print(p)  # product of the tree numbers on all the slopes
