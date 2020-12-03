3658825f2a24d19eeeed9ce44d0f381a86d2ef4a
Ewoud Dronkert <ewoud@dronkert.net>
2020-12-03 17:31:13 +0100

    simpler

--- day03.py
+++ day03.py
@@ -1,18 +1,17 @@
-from operator import add
-
 data = list(open('input03.txt'))
 width = len(data[0].strip())
 
-slopes = [[1,1], [3,1], [5,1], [7,1], [1,2]]
-prod = 1
+slope = [[1,1], [3,1], [5,1], [7,1], [1,2]]
+product = 1
 
-for slope in slopes:
-	pos = [0, 0]
-	sum = 0
-	while pos[1] < len(data):
-		sum += 1 if data[pos[1]][pos[0] % width] == '#' else 0
-		pos = list(map(add, pos, slope))
-	prod *= sum
-	print(slope, sum)
+for dx, dy in slope:
+	x = y = trees = 0
+	while y < len(data):
+		if data[y][x % width] == '#':
+			trees += 1
+		x += dx
+		y += dy
+	product *= trees
+	print(dx, dy, trees)
 
-print(prod)
+print(product)
