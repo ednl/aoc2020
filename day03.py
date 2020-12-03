0a507bff32f0c0ff99dbf27aac05ca6dcfa0872a
Ewoud Dronkert <ewoud@dronkert.net>
2020-12-03 17:35:22 +0100

    shorter

--- day03.py
+++ day03.py
@@ -7,10 +7,8 @@ product = 1
 for dx, dy in slope:
 	x = y = trees = 0
 	while y < len(data):
-		if data[y][x % width] == '#':
-			trees += 1
-		x += dx
-		y += dy
+		trees += 1 if data[y][x % width] == '#' else 0
+		x, y = x + dx, y + dy
 	product *= trees
 	print(dx, dy, trees)
 
