1739ced9ddc46d51274d25c64f4582af0065f194
Ewoud Dronkert <ewoud@dronkert.net>
2020-12-04 17:45:17 +0100

    tighter regexes

--- day04.py
+++ day04.py
@@ -1,10 +1,10 @@
 import re
 
 validregex = {
-	'byr': r'^(19[2-9]|200)\d$',
-	'iyr': r'^20[12]\d$',
-	'eyr': r'^20[23]\d$',
-	'hgt': r'^(1[5-9]\dcm|[5-7]\din)$',
+	'byr': r'^(19[2-9]\d|200[0-2])$',
+	'iyr': r'^20(1\d|20)$',
+	'eyr': r'^20(2\d|30)$',
+	'hgt': r'^(1([5-8]\d|9[0-3])cm|(59|6\d|7[0-6])in)$',
 	'hcl': r'^#[0-9a-f]{6}$',
 	'ecl': r'^(amb|blu|brn|gry|grn|hzl|oth)$',
 	'pid': r'^\d{9}$'
@@ -20,26 +20,8 @@ def isvalid(pp):
 	for k, regex in compiledregex.items():
 		if not (k in pp):
 			return 0
-		v = pp[k]
-		if not regex.match(v):
+		if not regex.match(pp[k]):
 			return 0
-		if k == 'byr':
-			i = int(v)
-			if i < 1920 or i > 2002:
-				return 0
-		elif k == 'iyr':
-			i = int(v)
-			if i < 2010 or i > 2020:
-				return 0
-		elif k == 'eyr':
-			i = int(v)
-			if i < 2020 or i > 2030:
-				return 0
-		elif k == 'hgt':
-			i = int(v[0:-2])
-			u = v[-2:]
-			if (u == 'cm' and (i < 150 or i > 193)) or (u =='in' and (i < 59 or i > 76)):
-				return 0
 	return 1
 
 data = list(open('input04.txt'))
