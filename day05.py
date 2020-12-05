b8aa4487ffc5d3c21c35ca57e6a5e00e2fcb06fd
Ewoud Dronkert <ewoud@dronkert.net>
2020-12-05 09:54:40 +0100

    generator expression

--- day05.py
+++ day05.py
@@ -1,11 +1,10 @@
-file = list(open('input05.txt'))
-
 hi = 0
 seats = []
-for line in file:
-	id = int(''.join(['1' if c == 'B' or c == 'R' else '0' for c in line.strip()]), 2)
-	hi = max(hi, id)
-	seats.append(id)
+with open('input05.txt') as f:
+	for line in (line.rstrip('\n') for line in f):
+		id = int(''.join(['1' if c == 'B' or c == 'R' else '0' for c in line]), 2)
+		hi = max(hi, id)
+		seats.append(id)
 
 # Part 1
 print(hi)
