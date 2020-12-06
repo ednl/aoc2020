e7271001c05c47d44b538b07d2cb54293b7489a6
Ewoud Dronkert <ewoud@dronkert.net>
2020-12-06 12:44:12 +0100

    no more notebook

--- day01.py
+++ day01.py
@@ -1,41 +1,44 @@
-#!/usr/bin/env python3
+with open('./input01.txt', 'r') as f:
+    a = sorted(map(int, list(f)))
 
-import numpy as np
-
-n = np.sort(np.loadtxt('./git/aoc2020/input01.txt', dtype=np.uint32))
-t = 2020
-
-i = 0
-while i < len(n) - 1:
-	j = i + 1
-	while j < len(n):
-		s = n[i] + n[j]
-		if s > t:
-			break
-		if s == t:
-			print("2:", n[i] * n[j])
-			break
-		j += 1
-	if s == t:
+print('Good')
+for i in range(len(a) - 2):
+	if a[i] >= 2020:
 		break
-	i += 1
+	else:
+		for j in range(i + 1, len(a) - 1):
+			sum2 = a[i] + a[j]
+			if sum2 > 2020:
+				break
+			elif sum2 == 2020:
+				print('2:', a[i] * a[j])
+				break
+			else:
+				for k in range(j + 1, len(a)):
+					sum3 = sum2 + a[k]
+					if sum3 > 2020:
+						break
+					elif sum3 == 2020:
+						print('3:', a[i] * a[j] * a[k])
+						break
 
-i = 0
-while i < len(n) - 2:
-	j = i + 1
-	while j < len(n) - 1:
-		k = j + 1
-		while k < len(n):
-			s = n[i] + n[j] + n[k]
-			if s > t:
-				break
-			if s == t:
-				print("3:", n[i] * n[j] * n[k])
-				break
-			k += 1
-		if s == t:
-			break
-		j += 1
-	if s == t:
-		break
-	i += 1
+print('\nBad, but correct')
+for i in a:
+	for j in a:
+		if (i + j == 2020):
+			print('2:', i * j)
+		for k in a:
+			if (i + j + k == 2020):
+				print('3:', i * j * k)
+
+print('\nBaaaaaaaad, but correct')
+from random import sample
+from functools import reduce
+
+for i in range(2, 4):
+	sub = []
+	n = 0
+	while n != 2020:
+		sub = sample(a, i)
+		n = sum(sub)
+	print(i, reduce((lambda x, y: x * y), sub))
