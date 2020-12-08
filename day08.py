62c90ea06d24b61ccb2811179aac41d2d3115701
Ewoud Dronkert <ewoud@dronkert.net>
2020-12-08 12:02:15 +0100

    commentary

--- day08.py
+++ day08.py
@@ -1,22 +1,21 @@
+# Initialise VM, memory size
 vm = []
-mem = 0
+memsize = 0
 
+# Build the VM from input
 with open('input08.txt') as f:
 	for str in (line.strip() for line in f):
-		vm.append(dict(op=str[:3], arg=int(str[4:]), addr=mem, ord=0))
-		mem += 1
-
-def flip(i):
-	if vm[i]['op'] == 'jmp':
-		vm[i]['op'] = 'nop'
-	elif vm[i]['op'] == 'nop':
-		vm[i]['op'] = 'jmp'
+		vm.append(dict(op=str[:3], arg=int(str[4:]), addr=memsize, ord=0))
+		memsize += 1
 
 def run():
+	# Initialise accumulator, instruction pointer, execution order
 	acc = ip = ord = 0
+	# Reset execution order state
 	for i in vm:
 		i['ord'] = 0
-	while ip >= 0 and ip < mem and vm[ip]['ord'] == 0:
+	# Run the VM until loop or out of bounds
+	while ip >= 0 and ip < memsize and vm[ip]['ord'] == 0:
 		ord += 1
 		vm[ip]['ord'] = ord
 		if vm[ip]['op'] == 'acc':
@@ -26,6 +25,7 @@ def run():
 			ip += vm[ip]['arg']
 		else:
 			ip += 1
+	# Return instruction pointer, accumulator
 	return ip, acc
 
 # Part 1
@@ -33,14 +33,29 @@ ip, acc = run()
 print(acc)
 
 # Part 2
+def flip(i):
+	if i >= 0 and i < memsize:
+		if vm[i]['op'] == 'jmp':
+			vm[i]['op'] = 'nop'
+		elif vm[i]['op'] == 'nop':
+			vm[i]['op'] = 'jmp'
+
+# Retain all jmp and nop instructions that were executed
 debug = [i for i in vm if i['ord'] > 0 and (i['op'] == 'jmp' or i['op'] == 'nop')]
-debug = sorted(debug, key=lambda i: i['ord'], reverse=True)
+
+# Sort in reverse execution order (probably good for speed)
+debug.sort(key=lambda i: i['ord'], reverse=True)
+
+# Save the memory addresses
 debug = [i['addr'] for i in debug]
 
+# Flip instructions at those addresses one by one
+# and re-run until no more loop
 for mempointer in debug:
 	flip(mempointer)
 	ip, acc = run()
-	if ip == mem:
+	if ip == memsize:
 		break
 	flip(mempointer)
+
 print(acc)
