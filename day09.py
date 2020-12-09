8a15d33bf70a68039efd15b87e8e2a1016f174f6
Ewoud Dronkert <ewoud@dronkert.net>
2020-12-09 14:32:38 +0100

    explanation

--- day09.py
+++ day09.py
@@ -22,30 +22,40 @@ for n in a[qlen:]:
 
 print(mynum)
 
-# Part 2, fastest I could think of
-s1 = a[0]
-for slicelen in range(2, len(a)):
-	i = 0
-	j = slicelen
-	s2 = s1 = s1 + a[j - 1]
-	while s2 != mynum and j < len(a):
-		s2 = s2 - a[i] + a[j]
-		i += 1
-		j += 1
-	if s2 == mynum:
-		print(min(a[i:j]) + max(a[i:j]))
-		break
+# Part 2: fastest
+# This first checks all starting points for a certain width,
+# then goes to the next width.
+def weakness1():
+	s1 = a[0]
+	for slicelen in range(2, len(a)):
+		i = 0
+		j = slicelen
+		s2 = s1 = s1 + a[j - 1]
+		while s2 != mynum and j < len(a):
+			s2 = s2 - a[i] + a[j]
+			i += 1
+			j += 1
+		if s2 == mynum:
+			return min(a[i:j]) + max(a[i:j])
+print(weakness1())
 
-# Part 2, not quite as fast
-for i in range(len(a) - 1):
-	s = a[i]
-	for j in range(i + 1, len(a)):
-		s += a[j]
-		if s == mynum:
-			print(min(a[i:j+1]) + max(a[i:j+1]))
-
-# Part 2, slowwwwww
-for i in range(len(a) - 1):
-	for j in range(i + 2, len(a) + 1):
-		if sum(a[i:j]) == mynum:
-			print(min(a[i:j]) + max(a[i:j]))
+# Part 2, simpler but about 2.5-3x as slow
+# This first checks all widths from a certain starting point,
+# then goes to the next starting point.
+def weakness2():
+	for i in range(len(a) - 1):
+		s = amin = amax = a[i]
+		for j in range(i + 1, len(a)):
+			s += a[j]
+			# Incorrect check because input is not sorted,
+			# but it still gives the right result for my input
+			# and makes it almost as fast as alternative 1.
+			# if s > mynum:
+			# 	break
+			if a[j] < amin:
+				amin = a[j]
+			if a[j] > amax:
+				amax = a[j]
+			if s == mynum:
+				return amin + amax
+print(weakness2())
