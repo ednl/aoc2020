82d080956a890ee06cc5dbb3046b58a7d0d8f5ac
Ewoud Dronkert <ewoud@dronkert.net>
2020-12-11 15:09:20 +0100

    improved by graph theory

--- day10.py
+++ day10.py
@@ -1,35 +1,22 @@
 from collections import Counter
 
-with open('input10.txt') as f:
-	a = sorted([int(line) for line in f])
+with open('input10.txt') as file:
+	data = [int(line) for line in file]
 
-a.append(a[-1] + 3)
-a.insert(0, 0)
-d = [a[i + 1] - a[i] for i in range(len(a) - 1)]
+data.append(0)
+data.sort()
+data.append(data[-1] + 3)
 
 # Part 1
-c = Counter(d)
+c = Counter([data[i + 1] - data[i] for i in range(len(data) - 1)])
 print(c[1] * c[3])
 
 # Part 2
-streak = []
-len = 0
-for i in d:
-	if i == 1:
-		len += 1
-	else:
-		if len > 1:
-			streak.append(len)
-		len = 0
-
-# Pairs of (k,v) where
-#   v = number of ways of summing up to k
-#   using any numbers from (1,2,3)
-#   where different order counts as a different way
-comb = {2: 2, 3: 4, 4: 7, 5: 13}
-
-p = 1
-for i in streak:
-	p *= comb[i]
-
-print(p)
+plen = data[-1] + 1
+paths = [0] * plen
+paths[0] = 1
+for i in range(1, plen):
+	for j in range(i - 3, i):
+		if j in data:
+			paths[i] += paths[j]
+print(paths[-1])
