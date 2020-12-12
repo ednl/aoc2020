290292a48f2a416d36e375fc4338c6e649530dba
Ewoud Dronkert <ewoud@dronkert.net>
2020-12-12 09:38:57 +0100

    rotations

--- day12.py
+++ day12.py
@@ -6,16 +6,15 @@ heading = {'E': (1,0), 'N': (0,1), 'W': (-1,0), 'S': (0,-1)}
 def nav(wx, wy, part):
 	x = y = 0
 	for action, value in data:
-		if action == 'L' or action == 'R':
-			if value == 180:
-				wx, wy = -wx, -wy
-			elif (action == 'L' and value == 90) or (action == 'R' and value == 270):
-				wx, wy = -wy, wx
-			elif (action == 'R' and value == 90) or (action == 'L' and value == 270):
-				wx, wy = wy, -wx
-		elif action == 'F':
+		if action == 'F':
 			x += value * wx
 			y += value * wy
+		elif action == 'L' or action == 'R':
+			if action == 'R':
+				value = 360 - value
+			while value:
+				wx, wy = -wy, wx
+				value -= 90
 		else:
 			dx, dy = [value * i for i in heading[action]]
 			if part == 1:
@@ -26,5 +25,5 @@ def nav(wx, wy, part):
 				wy += dy
 	return abs(x) + abs(y)
 
-print(nav(1, 0, 1))
-print(nav(10, 1, 2))
+print(nav(*heading['E'], part=1))
+print(nav(10, 1, part=2))
