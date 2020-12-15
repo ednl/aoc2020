0ddafaa652ec17ca893293a01061e8ccad3ee54e
Ewoud Dronkert <ewoud@dronkert.net>
2020-12-15 12:35:12 +0100

    Complete Van Eck

--- day15.py
+++ day15.py
@@ -1,9 +1,13 @@
-# Van Eck Missing Number
-# https://www.rosettacode.org/wiki/Van_Eck_sequence
 def play(game, turns):
-    seen = [0] * turns  # dumb but fast :(
+    if turns < 1:
+        return None
+    if len(game) == 0:
+        game = [0]
+    if turns <= len(game):
+        return game[turns - 1]
     lastnum = game[-1]
-    i = 0
+    seen = [0] * turns  # dumb but fast :(
+    i = 1
     while i < len(game):
         seen[game[i - 1]] = i
         i += 1
@@ -14,10 +18,20 @@ def play(game, turns):
         i += 1
     return lastnum
 
+# Van Eck sequence
+# https://www.rosettacode.org/wiki/Van_Eck_sequence
+print('Van Eck:', [play([], i) for i in range(1, 20)])
+
+# Test input
+# https://adventofcode.com/2020/day/15
+from itertools import permutations
+for game in permutations([1, 2, 3]):
+    print(game, 'x 2020 =', play(game, 2020))
+
 # Part 1
 game = [0,3,6]
-print(play(game, 2020))
+print('1:', play(game, 2020))
 
 # Part 2
 game = [13,16,0,12,15,1]
-print(play(game, 30000000))
+print('2:', play(game, 30000000))
