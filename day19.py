19d30e4cc389bfd4829130631c8e56300b43da7b
Ewoud Dronkert <ewoud@dronkert.net>
2020-12-19 15:21:46 +0100

    commentary

--- day19.py
+++ day19.py
@@ -4,6 +4,7 @@ isstr = re.compile(r'^(\d+): "([ab])"$')
 isnums = re.compile(r'^(\d+): (\d+)( \d+)?( \| (\d+)( \d+)?)?$')
 
 rules = {}
+cache = {}
 data = []
 
 part = 1
@@ -23,64 +24,51 @@ with open('input19.txt') as f:
         elif part == 2:
             data.append(line)
 
-def form(n, part=1):
-    if n in val:
-        return val[n]
-
-    if part == 2:
-        a = form(42)
-        val[42] = a
-        if n == 8:
-            a += '+'
-            val[8] = a
-            return a
-        elif n == 11:
-            b = form(31)
-            val[31] = b
-
-            # Testing:
-            # a += b                  # lower bound = 179
-            # a += '{2}' + b + '{2}'  # times 2     =  41 ; total = 220
-            # a += '{3}' + b + '{3}'  # times 3     =  15 ; total = 235
-            # a += '{4}' + b + '{4}'  # times 4     =   2 ; total = 237
-            # a += '{5}' + b + '{5}'  # times 5     =   0
-            # a += '+' + b + '+'      # upper bound = 245
-
-            # This works:
-            a = '(' + a + b + '|' + a + a + b + b + '|' + a + a + a + b + b + b + '|' + a + a + a + a + b + b + b + b + ')'
-
-            val[11] = a
-            return a
+def form(n):
+    if n in cache:
+        return cache[n]
 
     r = rules[n]
     if type(r) == str:
-        val[n] = r
+        cache[n] = r
         return r
 
-    a = form(r[0], part)
-    val[r[0]] = a
+    a = form(r[0])
+    cache[r[0]] = a
     if r[1] >= 0:
-        b = form(r[1], part)
-        val[r[1]] = b
+        b = form(r[1])
+        cache[r[1]] = b
         a += b
 
     if r[2] >= 0:
-        c = form(r[2], part)
-        val[r[2]] = c
+        c = form(r[2])
+        cache[r[2]] = c
         if r[3] >= 0:
-            d = form(r[3], part)
-            val[r[3]] = d
+            d = form(r[3])
+            cache[r[3]] = d
             c += d
         a = '(?:' + a + '|' +  c + ')'
 
     return a
 
 # Part 1
-val = {}
 rulezero = form(0)
 print(sum([1 if re.fullmatch(rulezero, message) else 0 for message in data]))
 
 # Part 2
-val = {}
-rulezero = form(0, 2)
-print(sum([1 if re.fullmatch(rulezero, message) else 0 for message in data]))
+#      0: 8 11
+#      8: 42 | 42 42 | 42 42 42 | ...
+#     11: 42 31 | 42 42 31 31 | 42 42 42 31 31 31 | ...
+# So, rule 0 = 42{n} 31{m} where n>m and m>0
+# Looking at captured subgroups: all matches for 42 or 31 have length 8,
+# so "n>m" can be translated as len(42{n}) > len(31{m})
+a = cache[42]
+b = cache[31]
+rulezero = '((?:' + a + ')+)((?:' + b + ')+)'
+n = 0
+for message in data:
+    fm = re.fullmatch(rulezero, message)
+    if fm:
+        if len(fm.group(1)) > len(fm.group(2)):
+            n += 1
+print(n)
