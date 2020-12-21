fb6b14e6624002aef6d994c21902f4df77f928ab
Ewoud Dronkert <ewoud@dronkert.net>
2020-12-21 23:01:58 +0100

    meh

--- day20-2.py
+++ day20-2.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.lib.function_base import place
 
 ids = []
 tiles = np.zeros((1,10,10), dtype=np.uint8)  #stub
@@ -11,13 +12,16 @@ with open('input20.txt') as f:
         tiles = np.concatenate((tiles, tile))
 tiles = tiles[1:,:,:]  # delete stub
 
-print(ids[0])
-print(tiles[0])
-
-# tiles = np.rot90(tiles, axes=(1, 2))
-# tiles = np.fliplr(tiles)
-
 # Original: t,r,b,l (=top,right,bottom,left)
 # Rotations: r,-b,l,-t | -b,-l,-t,-r | -l,t,-r,b
 # Flipped horizontally: -t,l,-b,r
 # Rotations: l,b,r,t | b,-r,t,-l | -r,-t,-l,-b
+
+print(tiles[0])
+tiles = np.rot90(tiles, axes=(1, 2))
+print(tiles[0])
+tiles = np.rot90(tiles, axes=(1, 2))
+print(tiles[0])
+
+place = np.zeros((1,1))
+# place = np.pad(place, ((1,1),), mode='constant')
