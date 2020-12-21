fb6b30689b8ae50c6c40b76390c9be17131d4ffc
Ewoud Dronkert <ewoud@dronkert.net>
2020-12-21 22:47:13 +0100

    more testing

--- day20-2.py
+++ day20-2.py
@@ -9,7 +9,15 @@ with open('input20.txt') as f:
         ids.append(int(t[0][5:-1]))
         tile = (np.array([[list(s) for s in t[1:]]]) == '#').astype(np.uint8)
         tiles = np.concatenate((tiles, tile))
-tiles = np.delete(tiles, 0, axis=0)  # delete stub
+tiles = tiles[1:,:,:]  # delete stub
+
+print(ids[0])
+print(tiles[0])
 
 # tiles = np.rot90(tiles, axes=(1, 2))
 # tiles = np.fliplr(tiles)
+
+# Original: t,r,b,l (=top,right,bottom,left)
+# Rotations: r,-b,l,-t | -b,-l,-t,-r | -l,t,-r,b
+# Flipped horizontally: -t,l,-b,r
+# Rotations: l,b,r,t | b,-r,t,-l | -r,-t,-l,-b
