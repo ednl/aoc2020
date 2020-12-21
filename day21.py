9ddea4badcdfdc8ea874df415d22d9beaa91f3e9
Ewoud Dronkert <ewoud@dronkert.net>
2020-12-21 14:34:13 +0100

    better variable names

--- day21.py
+++ day21.py
@@ -1,27 +1,29 @@
 possible = {}
-ingredients = {}
+ingredientcount = {}
 with open('input21.txt') as f:
     for line in f:
-        ingr, allerg = line.split(' (contains ')
-        ingr = set(ingr.split())
-        for i in ingr:
-            ingredients[i] = ingredients.get(i, 0) + 1
-        for a in allerg[:-2].split(', '):
+        ingredients, allergens = line.split(' (contains ')
+        ingredients = set(ingredients.split())
+        for i in ingredients:
+            ingredientcount[i] = ingredientcount.get(i, 0) + 1
+        for a in allergens[:-2].split(', '):
             if a in possible:
-                possible[a] &= ingr
+                possible[a] &= ingredients
             else:
-                possible[a] = ingr.copy()
+                possible[a] = ingredients.copy()
 
 danger = {}
 while len(possible):
     allergen = [a for a, i in possible.items() if len(i) == 1][0]
-    ingr = possible.pop(allergen)
+    ingredient = possible.pop(allergen)
     for a, i in possible.items():
-        i -= i & ingr
-    danger[allergen] = ingr.pop()
+        i -= i & ingredient
+    danger[allergen] = ingredient.pop()
 
 # Part 1
-print(sum([c for i, c in ingredients.items() if i not in danger.values()]))
+bad = danger.values()
+print(sum([c for i, c in ingredientcount.items() if i not in bad]))
 
 # Part 2
-print(','.join([i for _, i in sorted([(a, i) for a, i in danger.items()])]))
+sortbyallergen = sorted([(a, i) for a, i in danger.items()])
+print(','.join([i for _, i in sortbyallergen]))
