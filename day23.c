fafd8c8f1a3050d60afff68860a7c8e692cbc0ef
Ewoud Dronkert <ewoud@dronkert.net>
2020-12-23 21:14:19 +0100

    faster, pussycat

--- day23.c
+++ day23.c
@@ -1,63 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// #define CUPS         9u
-// #define MOVES      100u
-#define CUPS   1000000u
-#define MOVES 10000000u
-#define PICK         3u
-#define LABELS       9u
+// #define CUPCOUNT        9u
+// #define MOVES         100u
+#define CUPCOUNT  1000000u
+#define MOVES    10000000u
+#define PICK            3u
+#define LABELS          9u
 
-unsigned int a[CUPS + 1];
-unsigned int b[LABELS] = {3, 6, 2, 9, 8, 1, 7, 5, 4};
+unsigned int next[CUPCOUNT + 1];
+unsigned int cups[LABELS] = {3, 6, 2, 9, 8, 1, 7, 5, 4};
 
 int main(void)
 {
-    unsigned int i, j, cur, move, nxt, found;
+    unsigned int i, j, cur, movecount, ins, p1, p2, p3;
 
-    for (i = 0; i <= CUPS; ++i) {
-        a[i] = i + 1u;
+    for (i = 1; i < CUPCOUNT; ++i) {
+        next[i] = i + 1u;
     }
+    next[0] = next[CUPCOUNT] = cups[0];
 
-    a[0] = a[CUPS] = b[0];
     for (i = 0; i < LABELS - 1; ++i) {
-        a[b[i]] = b[i + 1];
+        next[cups[i]] = cups[i + 1];
     }
-    a[b[LABELS - 1]] = CUPS == LABELS ? b[0] : LABELS + 1;
+    next[cups[LABELS - 1]] = CUPCOUNT == LABELS ? cups[0] : LABELS + 1;
 
     cur = 0;
-    move = MOVES;
-    while (move--) {
-        cur = a[cur];
-        nxt = cur != 1 ? cur - 1 : CUPS;
-
-        do {
-            i = cur;
-            found = 0;
-            for (j = 0; j < PICK; ++j) {
-                if ((i = a[i]) == nxt) {
-                    found = 1;
-                    nxt = nxt != 1 ? nxt - 1 : CUPS;
-                    break;
-                }
-            }
-        } while (found);
-
-        j = a[i];
-        a[i] = a[nxt];
-        a[nxt] = a[cur];
-        a[cur] = j;
+    movecount = MOVES;
+    while (movecount--) {
+        cur = next[cur];
+        ins = cur - 1;
+        if (!ins)
+            ins = CUPCOUNT;
+        p1 = next[cur];
+        p2 = next[p1];
+        p3 = next[p2];
+        while (ins == p1 || ins == p2 || ins == p3) {
+            --ins;
+            if (!ins)
+                ins = CUPCOUNT;
+        }
+        j = next[p1];
+        next[p1] = next[ins];
+        next[ins] = next[cur];
+        next[cur] = j;
     }
 
-    if (CUPS < 10) {
-        i = a[1];
+    if (CUPCOUNT < 10) {
+        i = next[1];
         while (i != 1) {
             printf("%u", i);
-            i = a[i];
+            i = next[i];
         }
         printf("\n");
     } else {
-        printf("%lu\n", (unsigned long)a[1] * a[a[1]]);
+        printf("%lu\n", (unsigned long)next[1] * next[next[1]]);
     }
 
     return 0;
