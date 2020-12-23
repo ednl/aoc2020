2a433219daf50b6671373b6e51cac0aca4368041
Ewoud Dronkert <ewoud@dronkert.net>
2020-12-23 10:28:45 +0100

    linked list

--- day23.py
+++ day23.py
@@ -1,7 +1,7 @@
 data = '362981754'
 pick = 3
 
-def crabcup(moves):
+def crabcup1(moves):
     p = 0
     for _ in range(moves):
         cur = a[p]
@@ -38,7 +38,7 @@ def crabcup(moves):
 # Part 1
 a = [int(ch) - 1 for ch in data]
 mod = len(a)
-crabcup(100)
+crabcup1(100)
 s = ''
 p = (a.index(0) + 1) % mod
 for _ in range(mod - 1):
@@ -47,13 +47,28 @@ for _ in range(mod - 1):
 print(s)
 
 # Part 2
-# mod  mov1
-for mod in range(9, 10):
-    for mov in range(100, 101):
-        a = [i for i in range(mod)]
-        for i, ch in enumerate(data):
-            a[i] = int(ch) - 1
-        crabcup(mov)
-        p = (a.index(0) + 1) % mod
-        q = (p + 1) % mod
-        print(mod, mov, (a[p] + 1) * (a[q] + 1))
+mod = 9
+mov = 100
+
+a = [i for i in range(1, mod + 1)]
+a[mod - 1] = 0
+
+b = [int(c) - 1 for c in data]
+for i in range(len(b) - 1):
+    a[b[i]] = b[i + 1]
+a[b[-1]] = b[0]
+
+cur = b[0]
+nxt = mod - 1 if cur == 0 else cur - 1
+
+nextinpick = True
+while nextinpick:
+    nextinpick = False
+    i = a[cur]
+    for _ in range(pick):
+        if i == nxt:
+            nextinpick = True
+            nxt = mod - 1 if cur == 0 else nxt - 1
+            break
+        else:
+            i = a[i]
