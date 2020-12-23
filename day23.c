86e67ca4d41d1f4a8affc1d0ac4e110a0caaeeb3
Ewoud Dronkert <ewoud@dronkert.net>
2020-12-23 22:20:37 +0100

    var names

--- day23.c
+++ day23.c
@@ -1,41 +1,40 @@
 #include <stdio.h>
 
-#define CUPCOUNT  1000000u
-#define MOVES    10000000u
-#define PICK            3u
-#define LABELS          9u
+#define CUPS    1000000u
+#define MOVES  10000000u
+#define LABELS        9u
 
-unsigned int next[CUPCOUNT + 1];
+unsigned int next[CUPS + 1];
 unsigned int cups[LABELS] = {3, 6, 2, 9, 8, 1, 7, 5, 4};
 
 int main(void)
 {
-    unsigned int i, j, cur, movecount, ins, p1, p2, p3;
+    unsigned int i, j, cur, moves, ins, p1, p2, p3;
 
-    for (i = 1; i < CUPCOUNT; ++i) {
+    for (i = 1; i < CUPS; ++i) {
         next[i] = i + 1u;
     }
-    next[0] = next[CUPCOUNT] = cups[0];
+    next[0] = next[CUPS] = cups[0];
 
     for (i = 0; i < LABELS - 1; ++i) {
         next[cups[i]] = cups[i + 1];
     }
-    next[cups[LABELS - 1]] = CUPCOUNT == LABELS ? cups[0] : LABELS + 1;
+    next[cups[LABELS - 1]] = CUPS == LABELS ? cups[0] : LABELS + 1;
 
     cur = 0;
-    movecount = MOVES;
-    while (movecount--) {
+    moves = MOVES;
+    while (moves--) {
         cur = next[cur];
         ins = cur - 1;
         if (!ins)
-            ins = CUPCOUNT;
+            ins = CUPS;
         p1 = next[cur];
         p2 = next[p1];
         p3 = next[p2];
         while (ins == p1 || ins == p2 || ins == p3) {
             --ins;
             if (!ins)
-                ins = CUPCOUNT;
+                ins = CUPS;
         }
         j = next[p1];
         next[p1] = next[ins];
@@ -43,7 +42,7 @@ int main(void)
         next[cur] = j;
     }
 
-    if (CUPCOUNT < 10) {
+    if (CUPS < 10) {
         i = next[1];
         while (i != 1) {
             printf("%u", i);
