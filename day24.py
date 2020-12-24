18d13540cf18b3fdd8bec89b43e573e7269faeff
Ewoud Dronkert <ewoud@dronkert.net>
2020-12-24 15:57:05 +0100

    scipy version with convolution 9x faster

--- day24.py
+++ day24.py
@@ -1,100 +1,120 @@
 from copy import deepcopy
 
-#    0 1 2 3 4 5 6
-# 0: x x x x x x x
-# 1:  x x x x x x x
-# 2: x x x x x x x
-# 3:  x x x x x x x
-# 4: x x x x x x x
-
-# evenkernel = [
-#     [1, 1, 0],
-#     [1, 0, 1],
-#     [1, 1, 0]
-# ]
-# oddkernel = [
-#     [0, 1, 1],
-#     [1, 0, 1],
-#     [0, 1, 1]
-# ]
-
-step = [{
-    # step from even row
-    'e' : ( 0,  1),
-    'w' : ( 0, -1),
-    'ne': (-1,  0),
-    'nw': (-1, -1),
-    'se': ( 1,  0),
-    'sw': ( 1, -1),
-},
-{
-    # step from odd row
-    'e' : ( 0,  1),
-    'w' : ( 0, -1),
-    'ne': (-1,  1),
-    'nw': (-1,  0),
-    'se': ( 1,  1),
-    'sw': ( 1,  0),
-}]
-
-def next(r, c, id):
-    dr, dc = step[r % 2][id]
-    return r + dr, c + dc
-
-txt = []
-with open('input24.txt') as f:
-    txt = [line.strip() for line in f]
+#  r,c | 0 1 2 3 4 5 6
+# -----+--------------
+#  0   | x x x x x x x
+#  1   |  x x x x x x x
+#  2   | x x x x x x x
+#  3   |  x x x x x x x
+#  4   | x x x x x x x
 
 data = []
-i = 0
-for s in txt:
-    data.append([])
-    j = 0
-    while j < len(s):
-        ch = s[j]
-        if ch == 'e' or ch == 'w':
-            j += 1
-        else:
-            ch += s[j + 1]
-            j += 2
-        data[i].append(ch)
-    i += 1
+with open('input24.txt') as f:
+    i = 0
+    for line in f:
+        data.append([])
+        j = 0
+        s = line.strip()
+        while j < len(s):
+            ch = s[j]
+            if ch == 'e' or ch == 'w':
+                j += 1
+            else:
+                ch += s[j + 1]
+                j += 2
+            data[i].append(ch)
+        i += 1
 
-generations = 100
-maxdist = max(map(len, data))
-dim = 2 * (maxdist + generations) + 3
-mid = dim // 2
+# Determine grid size
+generations = 100  # for part 2
+maxdist = max(map(len, data))  # max number of steps per line, might be in one direction
+dim = 2 * (maxdist + generations) + 3  # distance can grow 1 for every generation 
+mid = dim // 2  # midpoint
 
 g1 = []
 for i in range(dim):
     g1.append([0] * dim)
 
-for nav in data:
-    r = c = mid
-    for bearing in nav:
-        r, c = next(r, c, bearing)
-    g1[r][c] ^= 1
+# Part 1
+# Steps for (row,col) from even row, odd row
+step = {
+    'e' : (( 0,  1), ( 0,  1)),
+    'w' : (( 0, -1), ( 0, -1)),
+    'ne': ((-1,  0), (-1,  1)),
+    'nw': ((-1, -1), (-1,  0)),
+    'se': (( 1,  0), ( 1,  1)),
+    'sw': (( 1, -1), ( 1,  0))
+}
 
-# Part 1 = 411
+def next(r, c, nav):
+    dr, dc = step[nav][r % 2]
+    return r + dr, c + dc
+
+for path in data:
+    r = c = mid
+    for nav in path:  # follow the complete path
+        r, c = next(r, c, nav)
+    g1[r][c] ^= 1  # flip the tile
+
 print(sum(map(sum, g1)))
 
-def neighbours(grid, r, c):
-    odd = r % 2
-    n = grid[r][c - 1] + grid[r][c + 1]
-    n += grid[r - 1][c + odd - 1] + grid[r - 1][c + odd]
-    n += grid[r + 1][c + odd - 1] + grid[r + 1][c + odd]
+# Part 2
+# import numpy as np
+# from scipy.signal import convolve2d
+
+# Kernels for even,odd rows
+# kernel = np.array([[
+#     [1, 1, 0],
+#     [1, 0, 1],
+#     [1, 1, 0]
+# ], [
+#     [0, 1, 1],
+#     [1, 0, 1],
+#     [0, 1, 1]
+# ]])
+
+# state = np.array(g1)
+# for _ in range(generations):
+#     nb0 = convolve2d(state, kernel[0], mode='same')
+#     nb1 = convolve2d(state, kernel[1], mode='same')
+#     state[::2] = state[::2] & (nb0[::2] == 1) | (nb0[::2] == 2)
+#     state[1::2] = state[1::2] & (nb1[1::2] == 1) | (nb1[1::2] == 2)
+
+# print(np.sum(state))
+
+# Kernels for even,odd rows
+kernel = [[
+    [1, 1, 0],
+    [1, 0, 1],
+    [1, 1, 0]
+], [
+    [0, 1, 1],
+    [1, 0, 1],
+    [0, 1, 1]
+]]
+
+def neighbours(g, k, r, c):
+    n = 0
+    r -= 1
+    c -= 1
+    for i in range(3):
+        kr = k[i]
+        gr = g[r + i]
+        for j in range(3):
+            n += kr[j] * gr[c + j]
     return n
 
 g2 = deepcopy(g1)
-for gen in range(generations):
-    for r in range(mid - maxdist - gen, mid + maxdist + gen + 1):
-        for c in range(mid - maxdist - gen, mid + maxdist + gen + 1):
-            nb = neighbours(g1, r, c)
-            if g1[r][c]:
-                g2[r][c] = 0 if nb == 0 or nb > 2 else 1
-            else:
-                g2[r][c] = 1 if nb == 2 else 0
+lim0 = mid - maxdist
+lim1 = mid + maxdist + 1
+for _ in range(generations):
+    for r in range(lim0, lim1):
+        k = kernel[r % 2]
+        for c in range(lim0, lim1):
+            nb = neighbours(g1, k, r, c)
+            g2[r][c] = 1 if nb == 2 or (nb == 1 and g1[r][c] == 1) else 0
     g1, g2 = g2, g1
+    lim0 -= 1
+    lim1 += 1
 
-# Part 2 = 4092
 print(sum(map(sum, g1)))
