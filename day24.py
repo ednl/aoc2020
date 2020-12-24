f1972e8bd80565a41777dcf435b36e34eaa504b0
Ewoud Dronkert <ewoud@dronkert.net>
2020-12-24 10:28:22 +0100

    faster, pussycat

--- day24.py
+++ day24.py
@@ -6,15 +6,39 @@ from copy import deepcopy
 # 2: x x x x x x x
 # 3:  x x x x x x x
 # 4: x x x x x x x
-def next(r, c, d):
-    if d == 'e':
-        return r, c + 1
-    if d == 'w':
-        return r, c - 1
-    dr = -1 if d[0] == 'n' else 1
-    dc = r % 2
-    if d[1] == 'w':
-        dc -= 1
+
+# evenkernel = [
+#     [1, 1, 0],
+#     [1, 0, 1],
+#     [1, 1, 0]
+# ]
+# oddkernel = [
+#     [0, 1, 1],
+#     [1, 0, 1],
+#     [0, 1, 1]
+# ]
+
+step = [{
+    # step from even row
+    'e' : ( 0,  1),
+    'w' : ( 0, -1),
+    'ne': (-1,  0),
+    'nw': (-1, -1),
+    'se': ( 1,  0),
+    'sw': ( 1, -1),
+},
+{
+    # step from odd row
+    'e' : ( 0,  1),
+    'w' : ( 0, -1),
+    'ne': (-1,  1),
+    'nw': (-1,  0),
+    'se': ( 1,  1),
+    'sw': ( 1,  0),
+}]
+
+def next(r, c, id):
+    dr, dc = step[r % 2][id]
     return r + dr, c + dc
 
 txt = []
@@ -36,17 +60,22 @@ for s in txt:
         data[i].append(ch)
     i += 1
 
+generations = 100
+maxdist = max(map(len, data))
+dim = 2 * (maxdist + generations) + 3
+mid = dim // 2
+
 g1 = []
-for i in range(301):
-    g1.append([0] * 301)
+for i in range(dim):
+    g1.append([0] * dim)
 
 for nav in data:
-    r = c = 150
+    r = c = mid
     for bearing in nav:
         r, c = next(r, c, bearing)
     g1[r][c] ^= 1
 
-# 411
+# Part 1 = 411
 print(sum(map(sum, g1)))
 
 def neighbours(grid, r, c):
@@ -57,9 +86,9 @@ def neighbours(grid, r, c):
     return n
 
 g2 = deepcopy(g1)
-for _ in range(100):
-    for r in range(1, 300):
-        for c in range(1, 300):
+for gen in range(generations):
+    for r in range(mid - maxdist - gen, mid + maxdist + gen):
+        for c in range(mid - maxdist - gen, mid + maxdist + gen):
             nb = neighbours(g1, r, c)
             if g1[r][c]:
                 g2[r][c] = 0 if nb == 0 or nb > 2 else 1
@@ -67,5 +96,5 @@ for _ in range(100):
                 g2[r][c] = 1 if nb == 2 else 0
     g1, g2 = g2, g1
 
-# 1899 too low
+# Part 2 = 4092
 print(sum(map(sum, g1)))
