22586870b4d63517f7358f7c2942fb7f17637dd1
Ewoud Dronkert <ewoud@dronkert.net>
2021-02-15 14:44:21 +0100

    Flipped the struct

--- day22.c
+++ day22.c
@@ -9,26 +9,28 @@
 static const unsigned char player1[] = {28, 50, 9, 11, 4, 45, 19, 26, 42, 43, 31, 46, 21, 40, 33, 20, 7, 6, 17, 44, 5, 39, 35, 27, 10};
 static const unsigned char player2[] = {18, 16, 29, 41, 14, 12, 30, 37, 36, 24, 48, 38, 47, 34, 15, 8, 49, 23, 1, 3, 32, 25, 22, 13, 2};
 
+// Yes, compiler, these *are* constant
 static const size_t P1_SIZE = sizeof player1 / sizeof *player1;
 static const size_t P2_SIZE = sizeof player2 / sizeof *player2;
 static const size_t MAXSIZE = P1_SIZE + P2_SIZE;
 
-typedef unsigned char DECK[MAXSIZE];
-typedef struct Game
-{
-    DECK deck[2];  // two circular buffers
-    size_t head[2];
-    size_t size[2];
-} GAME, *PGAME;
+// A deck is a stack of cards + index to the first card + size of the stack
+typedef struct {
+    unsigned char card[MAXSIZE];
+    size_t head;
+    size_t size;
+} DECK, *PDECK;
 
-static unsigned int deckid(PGAME pg, size_t player)
+// Score of a deck (also a fairly good state identifier)
+// calculate from bottom: 1 * value1 + 2 * value2 + 3 * value3 + ... + size * valueSize
+static unsigned int score(PDECK pd)
 {
     unsigned int id = 0;
-    size_t head = pg->head[player];
-    size_t size = pg->size[player];
+    size_t head = pd->head;
+    size_t size = pd->size;
 
     while (size) {
-        id += size-- * pg->deck[player][head++];
+        id += size-- * pd->card[head++];
         if (head == MAXSIZE) {
             head = 0;
         }
@@ -40,37 +42,37 @@ int main(void)
 {
     size_t i, winner;
     unsigned int rounds;
-    unsigned char card[2];
-    GAME game;
+    unsigned char draw[2];
+    DECK game[2];  // a game is 2 decks
 
     // Init game
-    memcpy(game.deck[0], player1, P1_SIZE);
-    game.head[0] = 0;
-    game.size[0] = P1_SIZE;
-    memcpy(game.deck[1], player2, P2_SIZE);
-    game.head[1] = 0;
-    game.size[1] = P2_SIZE;
+    memcpy(game[0].card, player1, P1_SIZE);
+    game[0].head = 0;
+    game[0].size = P1_SIZE;
+    memcpy(game[1].card, player2, P2_SIZE);
+    game[1].head = 0;
+    game[1].size = P2_SIZE;
 
     // Play game
     rounds = 0;
-    while (game.size[0] && game.size[1]) {
+    while (game[0].size && game[1].size) {
 
         // Draw 1 card from top of each deck
         for (i = 0; i < 2; ++i) {
-            card[i] = game.deck[i][game.head[i]++];
-            if (game.head[i] == MAXSIZE) {
-                game.head[i] = 0;
+            draw[i] = game[i].card[game[i].head++];
+            if (game[i].head == MAXSIZE) {
+                game[i].head = 0;
             }
-            game.size[i]--;
+            game[i].size--;
         }
 
         // Determine round winner from card values
         // true = 1 = player 1 wins, false = 0 = player 0 wins
-        winner = card[1] > card[0];
+        winner = draw[1] > draw[0];
 
         // Move 2 cards to bottom of winner's deck, highest first
         for (i = 0; i < 2; ++i) {
-            game.deck[winner][(game.head[winner] + game.size[winner]++) % MAXSIZE] = card[(winner + i) % 2];
+            game[winner].card[(game[winner].head + game[winner].size++) % MAXSIZE] = draw[(winner + i) % 2];
         }
 
         ++rounds;
@@ -78,8 +80,8 @@ int main(void)
 
     // Determine game winner from deck size
     // true = 1 = player 1 wins, false = 0 = player 0 wins
-    winner = game.size[0] == 0;
-    printf("%zu %u %u\n", winner, rounds, deckid(&game, winner));
+    winner = game[0].size == 0;
+    printf("%zu %u %u\n", winner, rounds, score(&game[winner]));
 
     return 0;
 }
