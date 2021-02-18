f21b564444888dafeb4df29329d7c27ee777a61d
Ewoud Dronkert <ewoud@dronkert.net>
2021-02-18 05:09:07 +0100

    prettier

--- day22.c
+++ day22.c
@@ -3,34 +3,35 @@
 #include <time.h>    // clock, difftime
 
 #define PLAYERS 2
-
-// Test
-// const unsigned char player1[] = {9, 2, 6, 3, 1};
-// const unsigned char player2[] = {5, 8, 4, 7, 10};
-
-#ifdef RPPICO
-// Puzzle
-static const unsigned char player1[] = {28, 50, 9, 11, 4, 45, 19, 26, 42, 43, 31, 46, 21, 40, 33, 20, 7, 6, 17, 44, 5, 39, 35, 27, 10};
-static const unsigned char player2[] = {18, 16, 29, 41, 14, 12, 30, 37, 36, 24, 48, 38, 47, 34, 15, 8, 49, 23, 1, 3, 32, 25, 22, 13, 2};
-// Yes, compiler, these *are* constant
-static const size_t P1_SIZE = sizeof player1 / sizeof *player1;
-static const size_t P2_SIZE = sizeof player2 / sizeof *player2;
-static const size_t MAXHAND = P1_SIZE + P2_SIZE;
-#else
 #define MAXHAND 50
 static const char *inp = "input22.txt";
-#endif
+// static const unsigned char player1[] = {28, 50, 9, 11, 4, 45, 19, 26, 42, 43, 31, 46, 21, 40, 33, 20, 7, 6, 17, 44, 5, 39, 35, 27, 10};
+// static const unsigned char player2[] = {18, 16, 29, 41, 14, 12, 30, 37, 36, 24, 48, 38, 47, 34, 15, 8, 49, 23, 1, 3, 32, 25, 22, 13, 2};
+// static const size_t P1_SIZE = sizeof player1 / sizeof *player1;
+// static const size_t P2_SIZE = sizeof player2 / sizeof *player2;
 
-// A hand is a stack of cards + index to the first card + size of the stack
 typedef struct {
     unsigned int size, head;
     unsigned char card[MAXHAND];
 } HAND, *PHAND;
 
-// Each player has a hand
-static HAND player[PLAYERS];
+static double timer(void)
+{
+    static unsigned int start = 1;
+    static struct timespec t0;
+    struct timespec t1;
 
-static void read()
+    if (start) {
+        clock_gettime(CLOCK_MONOTONIC, &t0);
+        start = 0;
+        return 0;
+    } else {
+        clock_gettime(CLOCK_MONOTONIC, &t1);
+        start = 1;
+        return 1.0 * t1.tv_sec + 1e-9 * t1.tv_nsec - (1.0 * t0.tv_sec + 1e-9 * t0.tv_nsec);
+    }
+}
+static void read(PHAND p)
 {
 	FILE *fp;
 	char *s = NULL;
@@ -41,35 +42,35 @@ static void read()
 	if ((fp = fopen(inp, "r")) != NULL) {
 		while (getline(&s, &t, fp) > 0 && i < PLAYERS) {
             if (s[0] == 'P') {
-                player[i].size = 0;
-                player[i].head = 0;
+                p[i].size = 0;
+                p[i].head = 0;
             } else if (s[0] == '\n') {
-                player[i++].size = j;
+                p[i++].size = j;
                 j = 0;
             } else if (j < MAXHAND) {
                 k = atoi(s);
                 if (k > 0 && k <= 255) {
-                    player[i].card[j++] = (unsigned char)k;
+                    p[i].card[j++] = (unsigned char)k;
                 }
             }
 		}
-        if (i < PLAYERS && j > player[i].size) {
-            player[i].size = j;
+        if (i < PLAYERS && j > p[i].size) {
+            p[i].size = j;
         }
 		free(s);
 		fclose(fp);
 	}
 }
 
-static void show()
+static void show(PHAND p)
 {
     unsigned int i, j, k;
 
     for (i = 0; i < PLAYERS; ++i) {
         printf("Player %i:", i + 1);
-        k = player[i].head;
-        for (j = 0; j < player[i].size; ++j) {
-            printf(" %u", player[i].card[k++]);
+        k = p[i].head;
+        for (j = 0; j < p[i].size; ++j) {
+            printf(" %u", p[i].card[k++]);
             if (k == MAXHAND) {
                 k = 0;
             }
@@ -79,14 +80,12 @@ static void show()
     printf("\n");
 }
 
-// Score of a deck (also a fairly good state identifier)
-// calculate from bottom: 1 * value1 + 2 * value2 + 3 * value3 + ... + size * valueSize
-static unsigned int score(PHAND ph)
+static unsigned int score(PHAND p)
 {
-    unsigned int id = 0, size = ph->size, head = ph->head;
+    unsigned int id = 0, size = p->size, head = p->head;
 
     while (size) {
-        id += size-- * ph->card[head++];
+        id += size-- * p->card[head++];
         if (head == MAXHAND) {
             head = 0;
         }
@@ -94,53 +93,42 @@ static unsigned int score(PHAND ph)
     return id;
 }
 
-int main()
+static unsigned int game1(PHAND p)
 {
-    unsigned int i, win, rounds, res;
+    unsigned int i, win;
     unsigned char draw[PLAYERS];
-    struct timespec t0, t1;
-    double dt;
 
-    // Start stopwatch
-    clock_gettime(CLOCK_MONOTONIC, &t0);
+    while (p[0].size && p[1].size) {
 
-    read();
-    show();
-
-    // Part 1
-    rounds = 0;
-    while (player[0].size && player[1].size) {
-
-        // Draw 1 card each
         for (i = 0; i < PLAYERS; ++i) {
-            draw[i] = player[i].card[player[i].head++];
-            if (player[i].head == MAXHAND) {
-                player[i].head = 0;
+            draw[i] = p[i].card[p[i].head++];
+            if (p[i].head == MAXHAND) {
+                p[i].head = 0;
             }
-            player[i].size--;
+            p[i].size--;
         }
-
-        // Determine round winner from card values
-        // true = 1 = player 1 wins, false = 0 = player 0 wins
         win = draw[1] > draw[0];
 
-        // Move 2 cards to bottom of winner's hand, highest first
         for (i = 0; i < PLAYERS; ++i) {
-            player[win].card[(player[win].head + player[win].size++) % MAXHAND] = draw[(win + i) % PLAYERS];
+            p[win].card[(p[win].head + p[win].size++) % MAXHAND] = draw[(win + i) % PLAYERS];
         }
-
-        ++rounds;
     }
 
-    // Determine game winner from hand size
-    // true = 1 = player 1 wins, false = 0 = player 0 wins
-    win = player[0].size == 0;
+    return p[1].size > p[0].size;
+}
+
+int main(void)
+{
+    HAND player[PLAYERS];
+    unsigned int win, res;
+
+    // Part 1
+    timer();
+    read(player);
+    show(player);
+    win = game1(player);
     res = score(&player[win]);
+    printf("winner: %u\nscore : %u\ntime  : %.6f s\n", win + 1, res, timer());
 
-    // Stop stopwatch
-    clock_gettime(CLOCK_MONOTONIC, &t1);
-    dt = 1.0 * t1.tv_sec + 1e-9 * t1.tv_nsec - (1.0 * t0.tv_sec + 1e-9 * t0.tv_nsec);
-
-    printf("winner: %u\nrounds: %u\nscore : %u\ntime  : %.6f s\n", win + 1, rounds, res, dt);
     return 0;
 }
