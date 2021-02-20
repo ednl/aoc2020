d122472239f45ad6c2ac5784d4977ec89b07f53c
Ewoud Dronkert <ewoud@dronkert.net>
2021-02-20 01:39:20 +0100

    shortcut

--- day22.py
+++ day22.py
@@ -1,11 +1,8 @@
-from copy import deepcopy
-# from math import prod
-
-with open('input22.txt') as f:
-    decks = [list(map(int, d.strip().split('\n')[1:])) for d in f.read().split('\n\n')]
+def read():
+    with open('input22.txt') as f:
+        return [list(map(int, d.strip().split('\n')[1:])) for d in f.read().split('\n\n')]
 
 def deckid(deck):
-    # return sum(map(prod, zip(range(len(deck), 0, -1), deck)))
     id = weight = 0
     for card in deck[::-1]:
         weight += 1
@@ -23,7 +20,7 @@ def combat1(decks):
         decks[w].extend([draw[w], draw[1 - w]])
     return sum(gameid(decks))
 
-print(combat1(deepcopy(decks)))
+print(combat1(read()))
 
 # Part 2
 def combat2(decks):
@@ -37,10 +34,14 @@ def combat2(decks):
             state.add(id)
         draw = [d.pop(0) for d in decks]
         if len(decks[0]) >= draw[0] and len(decks[1]) >= draw[1]:
-            w = combat2([decks[0][:draw[0]], decks[1][:draw[1]]])
+            if max(decks[0][:draw[0]]) > max(decks[1][:draw[1]]):
+                w = 0
+            else:
+                w = combat2([decks[0][:draw[0]], decks[1][:draw[1]]])
         else:
             w = 0 if draw[0] > draw[1] else 1
         decks[w].extend([draw[w], draw[1 - w]])
     return 0 if len(decks[0]) else 1
 
+decks = read()
 print(deckid(decks[combat2(decks)]))
