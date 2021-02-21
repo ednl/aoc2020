344f73fd8f4908408339ac549d17b922657ba852
Ewoud Dronkert <ewoud@dronkert.net>
2021-02-21 23:30:18 +0100

    better faster higher

--- day23.c
+++ day23.c
@@ -2,41 +2,45 @@
 #include <stdint.h>    // uint32_t, UINT32_C, uint64_t, UINT64_C
 #include <inttypes.h>  // PRIu32, PRIu64
 
-#define LABELS  (UINT32_C(9))
-#define CUPS    (UINT32_C(1000000))
-#define MOVES   (UINT32_C(10000000))
+#define LABELS   (UINT32_C(9))
+#define CUPCOUNT (UINT32_C(1000000))
+#define MOVES    (UINT32_C(10000000))
 
 static const uint_fast32_t init[LABELS] = {3, 6, 2, 9, 8, 1, 7, 5, 4};  // puzzle input
-static uint_fast32_t next[CUPS + 1];
+static uint_fast32_t next[CUPCOUNT + 1];  // index 0 not used, so, need one extra at the end
 
 int main(void)
 {
     uint_fast32_t i, j, cur, moves, ins, p1, p2, p3;
 
-    for (i = 1; i < CUPS; ++i) {
+    // Set up all cups in a circle (cup 0 is extra)
+    // where "next[1] = 2" means that cup 2 comes after cup 1
+    next[0] = next[CUPCOUNT] = init[0];
+    for (i = 1; i < CUPCOUNT; ++i) {
         next[i] = i + 1;
     }
-    next[0] = next[CUPS] = init[0];
 
+    // Use the puzzle input to jumble up the first few cups
     for (i = 0; i < LABELS - 1; ++i) {
         next[init[i]] = init[i + 1];
     }
-    next[init[LABELS - 1]] = CUPS == LABELS ? init[0] : LABELS + 1;
+    // And the last one; in a tight circle or are there more cups?
+    next[init[LABELS - 1]] = CUPCOUNT == LABELS ? init[0] : LABELS + 1;
 
-    cur = 0;
+    cur = 0;  // entry point to the first cup in the circle
     moves = MOVES;
     while (moves--) {
         cur = next[cur];
         ins = cur - 1;
         if (!ins)
-            ins = CUPS;
+            ins = CUPCOUNT;
         p1 = next[cur];
         p2 = next[p1];
         p3 = next[p2];
         while (ins == p1 || ins == p2 || ins == p3) {
             --ins;
             if (!ins)
-                ins = CUPS;
+                ins = CUPCOUNT;
         }
         j = next[p3];
         next[p3] = next[ins];
@@ -44,7 +48,7 @@ int main(void)
         next[cur] = j;
     }
 
-    if (CUPS < 10) {
+    if (CUPCOUNT < 10) {
         i = next[1];
         while (i != 1) {
             printf("%"PRIuFAST32, i);
