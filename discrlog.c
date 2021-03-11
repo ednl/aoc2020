2ec96d8c620b312aca30d73d4b356a39b06f8d09
Ewoud Dronkert <ewoud@dronkert.net>
2021-03-11 09:53:43 +0100

    more testing

--- discrlog.c
+++ discrlog.c
@@ -5,56 +5,56 @@
 #include <time.h>      // clock_gettime
 #include <math.h>      // sqrt
 
-#define BASE (UINT32_C(7)) 
-#define MOD  (UINT32_C(20201227))
+#define BASE (INT32_C(7)) 
+#define MOD  (INT32_C(20201227))
 
 typedef struct {
-    uint_fast32_t exp;
-    uint_fast32_t log;
+    int_fast32_t exp;
+    int_fast32_t log;
 } EXPLOG, *PEXPLOG;
 
-static double timer(void)
-{
-    static bool start = true;
-    static struct timespec t0;
-    struct timespec t1;
+// static double timer(void)
+// {
+//     static bool start = true;
+//     static struct timespec t0;
+//     struct timespec t1;
 
-    if (start) {
-        start = false;
-        clock_gettime(CLOCK_MONOTONIC_RAW, &t0);
-        return 0;
-    } else {
-        clock_gettime(CLOCK_MONOTONIC_RAW, &t1);
-        start = true;
-        return 1.0 * t1.tv_sec + 1e-9 * t1.tv_nsec - (1.0 * t0.tv_sec + 1e-9 * t0.tv_nsec);
-    }
-}
+//     if (start) {
+//         start = false;
+//         clock_gettime(CLOCK_MONOTONIC_RAW, &t0);
+//         return 0;
+//     } else {
+//         clock_gettime(CLOCK_MONOTONIC_RAW, &t1);
+//         start = true;
+//         return 1.0 * t1.tv_sec + 1e-9 * t1.tv_nsec - (1.0 * t0.tv_sec + 1e-9 * t0.tv_nsec);
+//     }
+// }
 
 // Naive discrete logarithm by trial multiplication
 // return e where BASE^e (mod MOD) = h
-static uint_fast32_t discrlog1(uint_fast32_t h)
+static void modlog(int_fast32_t arg, int_fast32_t base, int_fast32_t mod)
 {
-    uint_fast32_t e = 0, k = 1U;
-    while (k != h) {              // the target is h
-        k = k * BASE % MOD;       // all 32-bit numbers (BASE: 3bit, MOD: 25bit => k * BASE 28bit)
-        ++e;                      // exponent = multiplication count
+    int_fast32_t exp = 0, n = 1;
+    while (n != arg) {            // the target is 'arg'
+        n = n * base % mod;       // all 32-bit numbers (BASE: 3bit, MOD: 25bit => k * BASE 28bit)
+        ++exp;                    // exponent = multiplication count
     }
-    return e;
+    printf("%i ^ %i = %i (mod %i)\n\n", base, exp, arg, mod);
 }
 
 // Naive discrete logarithm by trial multiplication
 // return {e,p} where BASE^e (mod MOD) = p
 //     or {e,q} where BASE^e (mod MOD) = q
-// whichever e is smaller
-static EXPLOG discrlog2(uint_fast32_t p, uint_fast32_t q)
-{
-    uint_fast32_t e = 0, k = 1U;
-    while (k != p && k != q) {    // symmetry in p, q
-        k = k * BASE % MOD;       // all 32-bit numbers (BASE: 3bit, MOD: 25bit => k * BASE 28bit)
-        ++e;                      // exponent = multiplication count
-    }
-    return (EXPLOG){e, k == p ? p : q};   // use the other one for next calc
-}
+// for whichever e is smaller
+// static EXPLOG modlog2(int_fast32_t p, int_fast32_t q)
+// {
+//     int_fast32_t e = 0, k = 1U;
+//     while (k != p && k != q) {    // symmetry in p, q
+//         k = k * BASE % MOD;       // all 32-bit numbers (BASE: 3bit, MOD: 25bit => k * BASE 28bit)
+//         ++e;                      // exponent = multiplication count
+//     }
+//     return (EXPLOG){e, k == p ? p : q};  // but use the other one for next part of puzzle
+// }
 
 static void ext_gcd(int_fast32_t a, int_fast32_t b)
 {
@@ -66,15 +66,18 @@ static void ext_gcd(int_fast32_t a, int_fast32_t b)
         tmp = s0 - q * s; s0 = s; s = tmp;
         tmp = t0 - q * t; t0 = t; t = tmp;
     }
-    printf("Bezout coefficients     : %i %i\n", s0, t0);
-    printf("Greatest common divisor : %i\n", r0);
-    printf("Quotients by the gcd    : %i %i\n", t, s);
+    printf("Bezout  : %9i %9i\n", s0, t0);
+    printf("GCD     : %9i\n", r0);
+    printf("p,q/gcd : %9i %9i\n\n", t, s);
 }
 
-static void pollardrho(uint_fast32_t beta)
+// https://en.wikipedia.org/wiki/Pollard%27s_rho_algorithm_for_logarithms
+static void rholog(int_fast32_t alpha, int_fast32_t beta, int_fast32_t N)
 {
-    static const uint_fast32_t alpha = BASE, N = MOD, n = (MOD - 1);
-    uint_fast32_t i, x = 1, a = 0, b = 0, X = 1, A = 0, B = 0;
+    // static const int64_t N = MOD;
+    // static const int_fast32_t alpha = BASE, n = MOD - 1;
+    int64_t x = 1, X = 1;
+    int_fast32_t i, a = 0, b = 0, A = 0, B = 0, n = N - 1;
 
     for (i = 1; i < n; ++i) {
         switch (x % 3) {
@@ -94,23 +97,12 @@ static void pollardrho(uint_fast32_t beta)
         }
         if (x == X) break;
     }
-    printf("i,n   : %8u %8u\n", i, n);
-    printf("x,a,b : %8u %8u %8u\n", x, a, b);
-    printf("X,A,B : %8u %8u %8u\n", X, A, B);
+    printf("i,n,N   : %9i %9i %9i\n", i, n, N);
+    printf("x,a,b   : %9"PRId64" %9i %9i\n", x, a, b);
+    printf("X,A,B   : %9"PRId64" %9i %9i\n", X, A, B);
 
-    uint_fast32_t p, q;
-    if (B >= b) {
-        p = B - b;
-    } else {
-        p = n - b + B;
-    }
-    if (a >= A) {
-        q = a - A;
-    } else {
-        q = n - A + a;
-    }
-    printf("p,q   : %8u %8u\n", p, q);
-    ext_gcd(p, q);
+    printf("p,q     : %9i %9i\n", B - b, a - A);
+    ext_gcd(B - b, a - A);
 }
 
 // static uint_fast32_t babygiant(uint_fast32_t h)
@@ -120,38 +112,44 @@ static void pollardrho(uint_fast32_t beta)
 
 // }
 
-static uint_fast32_t nextpow2(uint_fast32_t num)
-{
-    uint_fast32_t count = 0;
-    while (num) {
-        num >>= 1;
-        ++count;
-    }
-    return (1 << count) % MOD;
-}
+// static uint_fast32_t nextpow2(uint_fast32_t num)
+// {
+//     uint_fast32_t count = 0;
+//     while (num) {
+//         num >>= 1;
+//         ++count;
+//     }
+//     return (1 << count) % MOD;
+// }
 
-static void result(uint_fast32_t p, uint_fast32_t q)
-{
-    int i, warmup = 3, loop = 100;
-    volatile uint_fast32_t r1 = {0}, r2 = {0};
-    double t, t1 = 0, t2 = 0, t1min = 10, t2min = 10, t1max = 0, t2max = 0;
+// static void result(uint_fast32_t p, uint_fast32_t q)
+// {
+//     int i, warmup = 3, loop = 100;
+//     volatile uint_fast32_t r1 = {0}, r2 = {0};
+//     double t, t1 = 0, t2 = 0, t1min = 10, t2min = 10, t1max = 0, t2max = 0;
 
-    for (i = 0; i < warmup; ++i) {
-        r1 = discrlog1(p);
-        r2 = discrlog1(q);
-    }
-    for (i = 0; i < loop; ++i) {
-        timer(); r1 = discrlog1(p); t = timer(); if (t < t1min) { t1min = t; } if (t > t1max) { t1max = t; } t1 += t;
-        timer(); r2 = discrlog1(q); t = timer(); if (t < t2min) { t2min = t; } if (t > t2max) { t2max = t; } t2 += t;
-    }
-    printf("  %"PRIu32" ^%8"PRIuFAST32" = %8"PRIuFAST32" (min %.5f avg %.5f max %.5f s)\n", BASE, r1, p, t1min, t1 / loop, t1max);
-    printf("  %"PRIu32" ^%8"PRIuFAST32" = %8"PRIuFAST32" (min %.5f avg %.5f max %.5f s)\n", BASE, r2, q, t2min, t2 / loop, t2max);
-}
+//     for (i = 0; i < warmup; ++i) {
+//         r1 = discrlog1(p);
+//         r2 = discrlog1(q);
+//     }
+//     for (i = 0; i < loop; ++i) {
+//         timer(); r1 = discrlog1(p); t = timer(); if (t < t1min) { t1min = t; } if (t > t1max) { t1max = t; } t1 += t;
+//         timer(); r2 = discrlog1(q); t = timer(); if (t < t2min) { t2min = t; } if (t > t2max) { t2max = t; } t2 += t;
+//     }
+//     printf("  %"PRIu32" ^%9"PRIuFAST32" = %9"PRIuFAST32" (min %.5f avg %.5f max %.5f s)\n", BASE, r1, p, t1min, t1 / loop, t1max);
+//     printf("  %"PRIu32" ^%9"PRIuFAST32" = %9"PRIuFAST32" (min %.5f avg %.5f max %.5f s)\n", BASE, r2, q, t2min, t2 / loop, t2max);
+// }
 
 int main(void)
 {
-    ext_gcd(23158, 166667);
-    // pollardrho(15113849);
+    modlog(15113849, BASE, MOD);
+    rholog(BASE, 15113849, MOD);
+
+    modlog(4206373, BASE, MOD);
+    rholog(BASE, 4206373, MOD);
+
+    modlog(5, 2, 1019);
+    rholog(2, 5, 1019);
 
     // printf("\nalgorithm (ednl)\n");
     // result(15113849, 4206373);
