27b01c63ecce8eecc208bf09fb957c9e74b98c6e
Ewoud Dronkert <ewoud@dronkert.net>
2021-03-13 18:54:31 +0100

    almost done

--- discrlog.c
+++ discrlog.c
@@ -6,8 +6,8 @@
 #include <time.h>      // clock_gettime
 #include <math.h>      // sqrt
 
-#define BASE (INT32_C(7)) 
-#define MOD  (INT32_C(20201227))
+// #define BASE (INT32_C(7)) 
+// #define MOD  (INT32_C(20201227))
 
 typedef struct {
     int_fast32_t exp;
@@ -57,9 +57,28 @@ static void modlog(int_fast32_t arg, int_fast32_t base, int_fast32_t mod)
 //     return (EXPLOG){e, k == p ? p : q};  // but use the other one for next part of puzzle
 // }
 
-static void ext_gcd(int_fast32_t a, int_fast32_t b)
+static int_fast32_t inverse(int_fast32_t a, int_fast32_t n)
 {
-    int_fast32_t q, tmp, r0 = a, r = b, s0 = 1, s = 0, t0 = 0, t = 1;
+    int_fast32_t q, tmp, t = 0, newt = 1, r = n, newr = a;
+
+    while (newr) {
+        q = r / newr;
+        tmp = t; t = newt; newt = tmp - q * newt;
+        tmp = r; r = newr; newr = tmp - q * newr;
+    }
+
+    if (r > 1) {
+        return -1;
+    }
+    if (t < 0) {
+        t += n;
+    }
+    return t;
+}
+
+static void ext_gcd(int64_t a, int64_t b)
+{
+    int64_t q, tmp, r0 = a, r = b, s0 = 1, s = 0, t0 = 0, t = 1;
 
     while (r) {
         q = r0 / r;
@@ -70,33 +89,28 @@ static void ext_gcd(int_fast32_t a, int_fast32_t b)
 
     // Correct signs
     if (r0 < 0) {
-        r0 = -r0;  // GCD > 0
+        r0 = -r0;  // GCD should not be negative
     }
     if ((a < 0) ^ (t < 0)) {
-        t = -t;    // t = reduced a, has same sign as a
+        t = -t;    // t = reduced a, should have same sign as a
     }
     if ((b < 0) ^ (s < 0)) {
-        s = -s;    // s = reduced b, has same sign as b
+        s = -s;    // s = reduced b, should have same sign as b
     }
 
-    printf("=> p,q  : %9i %9i\n", a, b);  // in the function call, a = B-b, b = n
-    printf("Bezout  : %9i %9i\n", s0, t0);
-    printf("GCD     : %9i\n", r0);
-    printf("p,q/gcd : %9i %9i\n\n", t, s);  // t = reduced a = (B-b)/gcd
-
-    // To find inverse of a, get Bézout coefficients for reduced a and reduced b
-    if (r0 != 1) {
-        ext_gcd(t, s);
-    }
+    printf("a,b     : %9"PRId64" %9"PRId64"\n", a, b);  // in the function call, a = B-b, b = n
+    printf("Bezout  : %9"PRId64" %9"PRId64"\n", s0, t0);
+    printf("GCD     : %9"PRId64"\n", r0);
+    printf("a,b/gcd : %9"PRId64" %9"PRId64"\n\n", t, s);  // t = reduced a = (B-b)/gcd, s = reduced b = n/gcd
 }
 
+// Calculate the discrete alpha'th-logarithm of beta (mod N)
+// i.e. find gamma where alpha ** gamma = beta (mod N)
 // https://en.wikipedia.org/wiki/Pollard%27s_rho_algorithm_for_logarithms
-static void rholog(int_fast32_t alpha, int_fast32_t beta, int_fast32_t N)
+static void rholog(int64_t alpha, int64_t beta, int64_t N)
 {
-    // static const int64_t N = MOD;
-    // static const int_fast32_t alpha = BASE, n = MOD - 1;
     int64_t x = 1, X = 1;
-    int_fast32_t i, a = 0, b = 0, A = 0, B = 0, n = N - 1;
+    int64_t i, a = 0, b = 0, A = 0, B = 0, n = N - 1;
 
     for (i = 1; i < n; ++i) {
         switch (x % 3) {
@@ -116,16 +130,24 @@ static void rholog(int_fast32_t alpha, int_fast32_t beta, int_fast32_t N)
         }
         if (x == X) break;
     }
-    printf("i,n,N   : %9i %9i %9i\n", i, n, N);
-    printf("x,a,b   : %9"PRId64" %9i %9i\n", x, a, b);
-    printf("X,A,B   : %9"PRId64" %9i %9i\n", X, A, B);
-    // Now: (B-b) . gamma = (a-A) (mod n)  where: alpha ^ gamma = beta (mod N)
+    printf("i,n,N   : %9"PRId64" %9"PRId64" %9"PRId64"\n", i, n, N);
+    printf("x,a,b   : %9"PRId64" %9"PRId64" %9"PRId64"\n", x, a, b);
+    printf("X,A,B   : %9"PRId64" %9"PRId64" %9"PRId64"\n", X, A, B);
+
+    int64_t p = B >= b ? B - b : n - b + B;
+    int64_t q = a >= A ? a - A : n - A + a;
+    printf("B-b,a-A : %9"PRId64" %9"PRId64"\n\n", p, q);
+
+    // Now: (B-b) . gamma = (a-A) (mod n)  where: alpha ** gamma = beta (mod N)
     // We must invert B-b (mod n) to get gamma. First, get the GCD of B-b and n:
-    ext_gcd(B - b, n);
+    ext_gcd(p, n);
     // And use this result: gamma = (a-A)/gcd . ((B-b)/gcd)^-1 (mod n/gcd)
-    // If the gcd was not 1, then calculate it again but this time to get Bézout's coefficients,
-    // where: s0 = (B-b)/gcd and t = n/gcd, from the previous ext_gcd() call.
-    // ext_gcd(s0, t);
+    // Because of Bézout's identity (B-b).x + n.y = gcd(B-b,n) <=> (B-b)/gcd . x + n/gcd . y = 1
+    // (mod n) it is inferred that the first Bézout coefficient x is de inverse of (B-b)/gcd
+    // thus gamma0 = q/r0 . s0 (mod s)  where: q = a-A, r0 = gcd, s0 = Bezout_x, s = n/gcd
+    // All solutions are gamma = (gamma0 + k . s) mod n  with k = 0..gcd-1
+    // One of them satisfies alpha ** gamma = beta (mod N).
+    // but which one??
 }
 
 // static uint_fast32_t babygiant(uint_fast32_t h)
@@ -165,17 +187,17 @@ static void rholog(int_fast32_t alpha, int_fast32_t beta, int_fast32_t N)
 
 int main(void)
 {
-    // modlog(15113849, BASE, MOD);
-    // rholog(BASE, 15113849, MOD);
+    // printf("%i\n", inverse(59994, 166667));
+    // printf("%i\n", inverse(359964, 1000002));
+
+    modlog(15113849, 7, 20201227);
+    rholog(7, 15113849, 20201227);
 
     // modlog(4206373, BASE, MOD);
     // rholog(BASE, 4206373, MOD);
 
-    // modlog(5, 2, 1019);
-    // rholog(2, 5, 1019);
-
-    modlog(262682, 5, 1000003);
-    rholog(5, 262682, 1000003);
+    // modlog(262682, 5, 1000003);
+    // rholog(5, 262682, 1000003);
 
     // printf("\nalgorithm (ednl)\n");
     // result(15113849, 4206373);
